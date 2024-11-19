b3fe92b49876a3e4c7589265ec644da61b4f738e
fokx <36614441+fokx@users.noreply.github.com>
2024-11-19 15:08:04 +0800

    auto focus input form after clicking reply button

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -6,6 +6,7 @@
 	import { fly, slide } from 'svelte/transition';
 	import User from '$lib/components/user.svelte';
 	import { enhance } from '$app/forms';
+	import { afterNavigate } from '$app/navigation';
 
 	let {
 		post = $bindable(),
@@ -27,28 +28,49 @@
 			repliesToThisPost = posts_local.filter(t => t.reply_to_post_id === post.id);
 		}
 	);
+	let inputElement;
 
+	function toggleReplyForm(event) {
+		show_reply_form = !show_reply_form;
+		var previousElementSibling = event.target.previousElementSibling;
+		console.log('previousElementSibling', previousElementSibling);
+		const ele = document.getElementById('post-reply-input');
+		ele?.focus();
+		// console.log('ele', ele);
+		if (show_reply_form) {
+			console.log('show_reply_form', show_reply_form);
+		}
+		if (inputElement) {
+			console.log('inputElement', inputElement);
+		}
+	}
+
+	/** @type {import('svelte/action').Action} */
+	function myaction(node) {
+		// the node has been mounted in the DOM
+
+		$effect(() => {
+			// setup goes here
+			console.log('node', node);
+			console.log('inputElement', inputElement);
+			node.focus();
+			if (inputElement) {
+				inputElement.focus();
+			}
+			return () => {
+				// teardown goes here
+			};
+		});
+	}
 </script>
 
-<!--{#if !post}-->
-<!--	<p style="color: red">error: post {post} is null</p>-->
-<!--{:else}-->
-<!--	<div style="display:inline; margin-right: 2px">-->
-<!--	</div>-->
+
 {#if indent === 0}
-	<div style="display: block; gap: 4em, 4em , 6em">
-		<User data={data} user_id={post.user_id} />
-	</div>
-	<div style="display:inline;">
+	<div class="original-post">
+		<div style="display: block; padding: 6px;">
+			<User data={data} user_id={post.user_id} />
+		</div>
 		<span>{post.text}</span>
-		{#if indent !== 0}
-			<div class="post-reply">
-				{#if !show_reply_form}
-					<button aria-label="toggle reply field" style="background: url(/reply.svg) no-repeat 50% 50%;"
-									class="filter-svg" onclick={()=>{show_reply_form=!show_reply_form}}></button>
-				{/if}
-			</div>
-		{/if}
 	</div>
 {/if}
 
@@ -64,7 +86,7 @@
 				<div class="post-reply">
 					{#if !show_reply_form}
 						<button aria-label="toggle reply field" style="background: url(/reply.svg) no-repeat 50% 50%;"
-										class="filter-svg" onclick={()=>{show_reply_form=!show_reply_form}}></button>
+										class="filter-svg" onclick={event => toggleReplyForm(event)}></button>
 					{/if}
 				</div>
 			{/if}
@@ -72,13 +94,15 @@
 	{/if}
 
 	{#if indent === 0 || show_reply_form}
-		<form class:is_first_post={post.is_main_post} action="?/create_reply" class="input-form" method="post" use:enhance={({
-																																																			formElement,
-																																																			formData,
-																																																			action,
-																																																			cancel,
-																																																			submitter
-																																																		}) => {
+		<form  id={indent === 0 ? undefined : 'post-reply-input'}
+					class:is_first_post={post.is_main_post}
+					action="?/create_reply" class="input-form"
+					method="post" use:enhance={({formElement,
+																			formData,
+																			action,
+																			cancel,
+																			submitter
+																		}) => {
 			// debounce the form submission
 			formElement.addEventListener('submit', (e) => {
 				e.preventDefault();
@@ -120,7 +144,15 @@
 				}
 			};
 		}}>
-			<input bind:value={newItem} name="content" placeholder="reply.." required type="text" />
+			{#if indent === 0}
+				<input bind:value={newItem} name="content" placeholder="reply.."
+							 required type="text" />
+				{:else}
+				<input use:myaction bind:this={inputElement} bind:value={newItem} name="content" placeholder="reply.."
+							 required type="text" />
+			{/if}
+			<div class="indent-line" style="left: -3px;"></div>
+
 			<button aria-label="Reply" disabled={!newItem} style="background: url(/reply.svg) no-repeat 50% 50%;"
 							class="filter-svg"></button>
 			{#if indent !== 0}
